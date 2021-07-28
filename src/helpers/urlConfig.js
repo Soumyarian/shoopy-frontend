803bf6a28efce7a109af94f76f66a4bff4882e05
Soumya Ranjan <padhysoumya98@gmail.com>
2021-07-28 09:23:25 +0530

    removing localhost from url

--- src/helpers/urlConfig.js
+++ src/helpers/urlConfig.js
@@ -1,6 +1,6 @@
 const baseUrl = window.location.hostname === "localhost" ? 'http://localhost:8000' : "https://shoppybackend.herokuapp.com"
 
-export const api = `${baseUrl}/http://localhost:8000/api`;
+export const api = `${baseUrl}/api`;
 
 export const generatePublicUrl = (fileName) => {
     return `${baseUrl}/public/${fileName}`
